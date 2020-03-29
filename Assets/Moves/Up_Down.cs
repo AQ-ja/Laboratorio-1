3b9f496a28a577547d2eb63b9eb24221b7cfd201
AQ-ja <53914116+AQ-ja@users.noreply.github.com>
2020-03-28 19:50:08 -0600

    ...

---
+++ Assets/Moves/Up_Down.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Up_Down : MonoBehaviour
+{
+    private Vector3 startPosition;
+    bool up = true;
+   // public float amplitud = 0; 
+    // Use this for initialization
+    void Start()
+    {
+        //maxSpeed = 3;
+        startPosition = transform.position;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        MoveVertical();
+    }
+    void MoveVertical()
+    {
+        var temp = transform.position;
+        print(up);
+        if (up == true)
+        {
+            temp.y += 0.010f;
+            transform.position = temp;
+            if (transform.position.y >= 3f)
+            {
+                up = false;
+            }
+        }
+        if (up == false)
+        {
+            temp.y -= 0.010f;
+            transform.position = temp;
+            if (transform.position.y <= 1.50f)
+            {
+                up = true;
+            }
+        }
+    }
+}
---
+++ Assets/Moves/WASD.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WASD : MonoBehaviour
+{
+    public float speed = 5;
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        transform.Translate(speed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, speed * Input.GetAxis("Vertical") * Time.deltaTime);
+    }
+}
